c82f278373e839b213369fee04f99ae4c83e01c1
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-07-14 20:08:10 +0100

    Moved all business logic to ViewModel and use observer to update the UI

--- app/src/main/java/com/mitchelletakuro/takurogbemisola/view/ui/filters/FilterListFragment.kt
+++ app/src/main/java/com/mitchelletakuro/takurogbemisola/view/ui/filters/FilterListFragment.kt
@@ -1,16 +1,24 @@
 package com.mitchelletakuro.takurogbemisola.view.ui.filters
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.mitchelletakuro.takurogbemisola.R
+import com.mitchelletakuro.takurogbemisola.data.repository.FilterRepo
+import com.mitchelletakuro.takurogbemisola.di.repoModule
+import org.koin.android.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FilterListFragment : Fragment() {
+
+    val viewModel: FilterViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +31,42 @@ class FilterListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+//        call to fetch data
+//        viewModel.getFilter()
+
+        viewModel.getPosts()
+
+        setUpObservers()
+
+    }
+
+    private fun setUpObservers() {
+        viewModel.loading.observe(viewLifecycleOwner, Observer{
+//
+            when(it){
+                true ->{
+                    // you can show a progress bar, loading would have started
+                }
+                false ->{
+                    // you can hide a progress bar, loading should be done here
+                }
+            }
+        })
+
+
+        viewModel.postList.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                Timber.e(it[0].toString())
+//                show/display the list as you deem fit
+            }
+        })
+
+
+        viewModel.error.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+//            you can add your own method to display errors to the user
+             Timber.e(it)
+//            showCustomErrorResponse(it)
+        })
     }
 }
---
+++ app/src/main/java/com/mitchelletakuro/takurogbemisola/view/ui/filters/FilterViewModel.kt
@@ -0,0 +1,51 @@
+package com.mitchelletakuro.takurogbemisola.view.ui.filters
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mitchelletakuro.takurogbemisola.data.models.PostsModel
+import com.mitchelletakuro.takurogbemisola.data.repository.FilterRepo
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+/**
+ * Created by manuelchris-ogar on 14/07/2020.
+ */
+class FilterViewModel(private val filterRepo: FilterRepo) : ViewModel() {
+
+
+//    this represents the data you will fetch, you can add more for the different types that this viewmodel will get
+    private val _postList = MutableLiveData<List<PostsModel>>()
+    val postList: LiveData<List<PostsModel>>
+        get() = _postList
+
+
+
+//     you can use this to display a Progress bar
+    val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = _loading
+
+
+// you can use this for errors
+    val _errors = MutableLiveData<String>()
+    val error: LiveData<String> get() = _errors
+
+
+    fun getPosts(){
+        _loading.postValue(true)
+        viewModelScope.launch {
+            try {
+                val posts = filterRepo.fetchPosts()
+                _loading.postValue(false)
+                _postList.postValue(posts)
+            }catch (e:Exception){
+                _errors.postValue(e.localizedMessage)
+            }
+
+
+        }
+    }
+
+}
